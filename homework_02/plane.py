62e777adfcdd2c84e7d92d93fe82816d50c63952
Alexander <134145027+totkin@users.noreply.github.com>
2023-06-29 19:16:24 +0300

    Add plane classes

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,33 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+
+"""
+- в модуле `plane` создайте класс `Plane`
+    - класс `Plane` должен быть наследником `Vehicle`
+    - добавьте атрибуты `cargo` и `max_cargo` классу `Plane`
+    - добавьте `max_cargo` в инициализатор (переопределите родительский)
+    - объявите метод `load_cargo`, который принимает число, проверяет, что в сумме с текущим `cargo` не будет перегруза, и обновляет значение, в ином случае выкидывает исключение `exceptions.CargoOverload`
+    - объявите метод `remove_all_cargo`, который обнуляет значение `cargo` и возвращает значение `cargo`, которое было до обнуления
+"""
+from homework_02.base import Vehicle
+from homework_02.exceptions import CargoOverload
+
+
+class Plane(Vehicle):
+    cargo: int
+    max_cargo: int
+
+    def __init__(self, mc):
+        self.max_cargo = mc
+
+    def load_cargo(self, num):
+        if self.cargo + num < self.max_cargo:
+            self.cargo += num
+        else:
+            raise CargoOverload
+
+    def remove_all_cargo(self):
+        b = self.cargo
+        self.cargo = 0
+        return b
