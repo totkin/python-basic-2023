c0346e197828b1905473dd25d3f55d877123cbbf
Alexander <134145027+totkin@users.noreply.github.com>
2023-08-18 19:27:39 +0300

    05 main.py refresh

--- homework_03/main.py
+++ homework_03/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "ROOT"}
 
 
@@ -15,4 +15,4 @@ def ping():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)  #
+    uvicorn.run("main:app", host='0.0.0.0', port=8000)  #
