90975cc5022e165aadac3d64f8719878f3cadaf1
totkin <test4git@gmail.com>
2023-10-08 23:50:53 +0300

    import some files with sql

--- homework_07/sql/Start scripts 20231008.sql
+++ homework_07/sql/Start scripts 20231008.sql
@@ -17,7 +17,36 @@ VALUES(6, 'another name');
 select * from AODATA.REPORT.test20231008
 
 
-CREATE SCHEMA [nomad] AUTHORIZATION [nomaduser@s-sql5]
+CREATE SCHEMA [nomad] AUTHORIZATION [nomaduser]
 
 
 alter USER [nomaduser] WITH DEFAULT_SCHEMA =[nomad]
+
+
+GRANT CONNECT TO [nomaduser]
+
+
+GRANT INSERT ON SCHEMA :: nomad TO nomaduser
+
+
+select CURRENT_USER 
+
+
+ALTER LOGIN nomad ENABLE
+
+
+SELECT pr.principal_id
+    ,pr.name
+    ,pr.type_desc
+    ,pr.authentication_type_desc
+    ,pe.state_desc
+    ,pe.permission_name  
+FROM sys.database_principals AS pr  
+INNER JOIN sys.database_permissions AS pe ON pe.grantee_principal_id = pr.principal_id;
+
+
+
+
+Create login nomaduser with password='pa$$w0rd';
+Create user nomaduser for login nomaduser;
+Grant Execute to nomaduser
